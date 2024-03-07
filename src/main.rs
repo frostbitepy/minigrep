1a11e41f5265dc0adabc259830fa994731f37dc3
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2024-03-07 10:50:38 -0300

    Developed the lib functionality with Test-Driven Development

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     });
     
     println!("Searching for '{}'", config.query);
-    println!("In file {}", config.file_path); 
+        println!("In file {}", config.file_path); 
     
     if let Err(e) = minigrep::run(config) {
         println!("Aplication error: {e}");
