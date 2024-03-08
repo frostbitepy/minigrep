bbfc3e4b6e8779e3512b33b8b11b9ec2effa9372
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2024-03-08 11:53:19 -0300

    Implemented writing error messages to standard error

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsin arguments: {}", err);
+        eprintln!("Problem parsin arguments: {}", err);
         process::exit(1);
     });
     
-    println!("Searching for '{}'", config.query);
-        println!("In file {}", config.file_path); 
-    
     if let Err(e) = minigrep::run(config) {
-        println!("Aplication error: {e}");
+        eprintln!("Aplication error: {e}");
         process::exit(1);
     }
 }
